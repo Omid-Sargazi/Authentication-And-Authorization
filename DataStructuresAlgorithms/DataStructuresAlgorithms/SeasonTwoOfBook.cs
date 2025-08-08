9d57e10fa46ea4da107f8b34bcb46204410f9637
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-08 22:19:58 +0330

    .

--- DataStructuresAlgorithms/DataStructuresAlgorithms/SeasonTwoOfBook.cs
+++ DataStructuresAlgorithms/DataStructuresAlgorithms/SeasonTwoOfBook.cs
@@ -22,6 +22,30 @@ namespace DataStructuresAlgorithms
             Type t = arr.GetType();
             Console.WriteLine(t.IsArray);
             Console.WriteLine(t);
+
+
+            int[,] grades = new int[4, 5]; // row is 4 and colum is 5
+
+            int[,] g = new int[,]{
+                {1,2,3,4,5},
+                {1,2,3,4,5},
+                {1,2,3,4,5},
+                {1,2,3,4,5},
+            };
+
+            int rows = g.GetLength(0);
+            int cols = g.GetLength(1);
+
+            for (int i = 0; i <= rows - 1; i++)
+            {
+                int total = 0;
+                for (int j = 0; j <= cols - 1; j++)
+                {
+                    total += g[i, j];
+                    double avg = (double)total / cols;
+                    Console.WriteLine($"Row:{i}:avg:{avg:F2}");
+                }
+            }
         }
     }
 }
