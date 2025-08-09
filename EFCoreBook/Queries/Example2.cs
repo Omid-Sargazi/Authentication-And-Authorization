10f00f99a08c0016ea6e050b524ce1cb997419ae
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-09 21:27:36 +0330

    Lazy Loading With Calculate Time for lazy

--- EFCoreBook/Queries/Example2.cs
+++ EFCoreBook/Queries/Example2.cs
@@ -1,5 +1,7 @@
+using System.Diagnostics;
 using EFCoreBook.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EFCoreBook.Queries
 {
@@ -15,13 +17,31 @@ namespace EFCoreBook.Queries
             // Console.WriteLine($"Orders: {totalOrders}, Items: {totalItems}");
 
 
+            // var options = new DbContextOptionsBuilder<AppDbContext>()
+            // .UseInMemoryDatabase("PerfDemoDb")
+            // .UseLazyLoadingProxies()
+            // .Options;
+
             var options = new DbContextOptionsBuilder<AppDbContext>()
-            .UseInMemoryDatabase("PerfDemoDb")
+            .UseSqlite("DataSource=memory")
             .UseLazyLoadingProxies()
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
             .Options;
 
+
+
             using var context = new AppDbContext(options);
+            context.Database.OpenConnection();
+            context.Database.EnsureCreated();
+
             var customers = SeedData.GenerateCustomers(customers: 50, ordersPerCustomer: 5, itemsPerOrder: 5);
+
+            // var allCustomers = context.Customers.ToList();
+            // foreach (var c in allCustomers)
+            // {
+            //     Console.WriteLine($"Customer: {c.Name}, Orders: {c.Orders.Count}");
+            // }
 
             context.Customers.AddRange(customers);
             context.SaveChanges();
@@ -31,18 +51,20 @@ namespace EFCoreBook.Queries
             Console.WriteLine($"Orders in DB: {context.Orders.Count()}");
             Console.WriteLine($"Items in DB: {context.Items.Count()}");
 
+            var sw = Stopwatch.StartNew();
+
 
             var allCustomers = context.Customers.ToList();
             foreach (var c in allCustomers)
             {
                 Console.WriteLine($"Customer:{c.Name},Order:{c.Orders.Count}");
             }
+            sw.Stop();
+            Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds} ms");
+                
+
+
             
-
-
-
-
-
 
         }
     }
